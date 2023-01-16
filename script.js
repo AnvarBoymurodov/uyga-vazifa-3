8591974d1ec6cafe1904f1b74e9b3f9323f8c05b
Anvar Boymurodov <anvarboymurodov47.com>
2023-01-16 18:00:36 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let namePrompt = prompt( "Ismingiz nima?")
+let surnamePrompt = prompt( "familiyangiz nima?")
+let agePrompt = prompt( "Yoshingiz nechchida?")
+
+let user = {
+    name: namePrompt,
+    surname:surnamePrompt,
+    age:agePrompt
+};
+
+console.log(user)
+
+console.log(user.name)
+console.log(user.surname)
+console.log(user.age)
